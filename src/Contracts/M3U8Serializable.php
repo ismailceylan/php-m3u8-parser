cbe5314033de9500a7a8f7444a5a80de7cd3131d
Ismail Ceylan <ismaillceylan@gmail.com>
2025-04-10 13:47:42 +0300

    docs: add missing PHPDoc block

--- src/Contracts/M3U8Serializable.php
+++ src/Contracts/M3U8Serializable.php
@@ -2,7 +2,18 @@
 
 namespace Iceylan\M3U8\Contracts;
 
+/**
+ * Interface for objects that can be serialized in the M3U8 format.
+ *
+ * Implementing classes must provide a toM3U8 method that returns a string
+ * representation of the object in the M3U8 format.
+ */
 interface M3U8Serializable
 {
+	/**
+	 * Returns a string representation of the object in the M3U8 format.
+	 *
+	 * @return string
+	 */
 	public function toM3U8(): string;
 }
